f15cf45577596293444c3a1b6bbf343e09cbec1c
User <user@Users-MacBook-Pro.local>
2024-07-25 23:00:46 -0400

    server, mongodb connection

---
+++ index.js
@@ -0,0 +1,23 @@
+const {ApolloServer} = require('apollo-server');
+const mongoose = require('mongoose');
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
+
+const MONGODB = 'mongodb+srv://DanyP:Irjkf1018ujdyj!@cluster0.w1izgai.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+
+})
+
+mongoose.connect(MONGODB, {useNewUrlParser: true})
+    .then(() => {
+        console.log("MongoDB Connection successful");
+        return server.listen({port: 5000})
+    })
+    .then((res) => {
+        console.log(`Server running at ${res.url}`);
+    })
+
+
